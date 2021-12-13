ba013ef0c250dc9852378173952337be56ba0726
msjun23 <msjun23@gmail.com>
2021-12-13 23:00:34 +0900

    Update print format

--- f1tenth-riders-quickstart-master2/pkg/src/image_coor_info.py
+++ f1tenth-riders-quickstart-master2/pkg/src/image_coor_info.py
@@ -12,10 +12,12 @@ def Sim2ImgCoordinate(sim_coordinate):
 
 def onMouse(event, x, y, flags, param):
     if event==cv2.EVENT_LBUTTONDOWN:
-        print('왼쪽 마우스 클릭 했을 때 좌표: ', x, y)
-        print('Sim 좌표: ', Img2SimCoordinate([x, y]))
+        point = [x*2, y*2]
+        print('왼쪽 마우스 클릭 했을 때 좌표: ', point)
+        print('Sim 좌표: ', Img2SimCoordinate([x*2, y*2]))
 
 img = cv2.imread('pkg/maps/ROBOT_NAVIGATION.png')
+img = cv2.resize(img, dsize=(1000, 1000), interpolation=cv2.INTER_AREA)
 
 cv2.imshow('image', img)
 cv2.setMouseCallback('image', onMouse)
